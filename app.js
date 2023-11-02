ce644b646a8e99fb1234c1d5134515e063024b3f
Arish Imam <62866346+arishimam@users.noreply.github.com>
2023-11-02 15:21:35 -0700

    somewhat working

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
 let gameFinished = false;
 let currentPlayer = 1;
+let boardIndex = 0;
 
 const player1_moves = new Set(["pit1", "pit2", "pit3", "pit4", "pit5", "pit7"]);
 
@@ -20,6 +21,9 @@ function switchTurn() {
 }
 function isValidMove(pit) {
   // Logic to check if valid move
+  if (board[boardIndex] < 1) {
+    return false;
+  }
   if (currentPlayer == 1 && player1_moves.has(pit)) {
     return true;
   }
@@ -27,6 +31,36 @@ function isValidMove(pit) {
     return true;
   }
   return false;
+}
+
+function setIndex(pit) {
+  pit = parseInt(pit.replace("pit", ""));
+  console.log(pit);
+  if (pit <= 6) {
+    boardIndex = pit - 1;
+  } else {
+    boardIndex = pit;
+  }
+  console.log(`Board Index: ${boardIndex}`);
+}
+
+function makeMove() {
+  let skipIndex = 13;
+  if (currentPlayer == 2) {
+    skipIndex = 6;
+  }
+  numPebbles = board[boardIndex];
+  board[boardIndex] = 0;
+
+  while (numPebbles > 0) {
+    numPebbles -= 1;
+    boardIndex += 1;
+    if (boardIndex != skipIndex) {
+      board[boardIndex] += 1;
+    }
+    updateBoard();
+  }
+  console.log(board);
 }
 
 function updateBoard() {
@@ -51,8 +85,10 @@ document.querySelectorAll(".pit").forEach((pit) => {
     // Figure out if turn switches or bonus turn granted
     // Update board and UI
     console.log(pit.id);
+    setIndex(pit.id);
     if (isValidMove(pit.id)) {
       console.log("valid move!");
+      makeMove();
       switchTurn();
     } else {
       console.log("invalid move");
